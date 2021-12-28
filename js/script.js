a49a2a7a1556ce1f2b5c78fda914329398ed628b
Nikita <nsotnikov16@yandex.ru>
2021-12-28 11:43:35 +0500

    Updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // Swiper Banners 
 var swiperBanners = new Swiper(".swiper-banner", {
-    
+
     autoplay: {
         delay: 4000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
     },
     navigation: {
         nextEl: ".swiper-banner .swiper-button-next",
@@ -26,8 +30,38 @@ var swiperBanners = new Swiper(".swiper-banner", {
                 })
             }, 50) */
         },
-        init: function() {
+        init: function () {
             /* document.querySelector('.swiper-banner .swiper-slide ').classList.add('_animate') */
         }
     },
 });
+
+
+class Form {
+    constructor(element) {
+        this._form = element;
+        this._rows = Array.from(this._form.querySelectorAll('.form-row'));
+        this._inputs = Array.from(this._form.querySelectorAll('.form-input'));
+        this._labels = Array.from(this._form.querySelectorAll('.form-label'));
+        this.setEventListeners()
+    }
+
+    activateInput(row) {
+        row.classList.add('none')
+    }
+
+    deactivateInput(row) {
+        row.classList.remove('none')
+    }
+
+    setEventListeners() {
+        this._labels.forEach(label => label.addEventListener('click', () => this.activateInput(label.closest('.form-row'))))
+        document.addEventListener('click', (e) => {
+            const row = this._rows.find(row => row.classList.contains('none'))
+            if (row && (e.target.parentNode !== row)) this.deactivateInput(row)
+        })
+    }
+}
+
+const forms = document.querySelectorAll('.form')
+if (forms.length > 0) forms.forEach(form => new Form(form))
