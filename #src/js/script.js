1daf0ad40a1819cf89c300079a1d9120511a1327
Nikita <nsotnikov16@yandex.ru>
2021-12-28 17:52:54 +0500

    update

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,4 @@
-// Swiper Banners 
+// Swiper Banner 
 var swiperBanners = new Swiper(".swiper-banner", {
 
     autoplay: {
@@ -36,7 +36,7 @@ var swiperBanners = new Swiper(".swiper-banner", {
     },
 });
 
-
+// Forms
 class Form {
     constructor(element) {
         this._form = element;
@@ -52,9 +52,7 @@ class Form {
     }
 
     deactivateInput(row) {
-        /* this._rows.forEach(row => row.classList.remove('none')) */
         row.classList.remove('none')
-        
     }
 
     setEventListeners() {
@@ -64,8 +62,94 @@ class Form {
             const row = this._rows.find(row => row.classList.contains('none'))
             if (row && (e.target.parentNode !== row)) this.deactivateInput(row)
         })
+
+        this._inputs.forEach(input => input.addEventListener('focus', () => {
+            if (!input.closest('.banner__form')) input.closest('.form-row').style.border = "2px solid rgba(229, 41, 41, 0.4)"
+        }))
+        this._inputs.forEach(input => input.addEventListener('focusout', () => {
+            if (!input.closest('.banner__form')) input.closest('.form-row').style.border = "2px solid #e4ebf1"
+        }))
     }
 }
 
 const forms = document.querySelectorAll('.form')
 if (forms.length > 0) forms.forEach(form => new Form(form))
+
+
+// YMaps
+ymaps.ready(init);
+
+const addresses = [
+    {
+        id: "1",
+        coordinates: [61.264919, 73.416461],
+        title: "Магазин",
+        adress: "г. Сургут, Профсоюзов, 39/2",
+        /* idMap: "79382519486" */
+    },
+    {
+        id: "2",
+        coordinates: [61.267164, 73.408331],
+        title: "Автосервис",
+        adress: "г. Сургут, Профсоюзов, 31/2",
+        /* idMap: "34832144675" */
+    },
+
+];
+
+function init() {
+    var myMap = new ymaps.Map(
+        "map",
+        {
+            center: [61.265108, 73.412632],
+            zoom: 15,
+            controls: ['zoomControl', 'geolocationControl'],
+        },
+        {
+            searchControlProvider: "yandex#search",
+            zoomControlPosition: { right: 10, top: 250 },
+            zoomControlSize: 'small',
+            geolocationControlPosition: { right: 10, top: 330 }
+        }
+    ),
+        BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+            `<div class="map__info">
+                <p class="map__info-title">{{properties.title}}</p>   
+                <div class="map__info-row">
+                    <svg>
+                        <use href="./img/icons/icons.svg#map"></use>
+                    </svg>
+                    <p>{{properties.adress}}</p>
+                </div>
+                <a href="tel:+73452355560" class="map__phone">
+                    <svg>
+                        <use href="./img/icons/icons.svg#phone-footer"></use>
+                    </svg>
+                    <p>+7 (3462) 769-382</p>
+                </a>
+            </div>`
+        );
+
+    addresses.forEach(
+        ({ adress, title, coordinates, id }, ind) => {
+            myPlacemarkWithContent = new ymaps.Placemark(
+                coordinates,
+                {
+                    adress,
+                    title
+                },
+                {
+                    iconImageHref: "./img/geo.svg",
+                    iconImageSize: [55, 60],
+                    iconLayout: "default#imageWithContent",
+                    iconContentOffset: [12.5, 10],
+                    iconImageOffset: [-25, 0],
+                    balloonContentLayout: BalloonContentLayout,
+                    balloonPanelMaxMapArea: 0,
+                    hideIconOnBalloonOpen: false,
+                }
+            );
+
+            myMap.geoObjects.add(myPlacemarkWithContent);
+        })
+}
